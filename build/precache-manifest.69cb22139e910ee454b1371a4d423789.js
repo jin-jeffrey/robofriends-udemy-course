1925a9d309ede51c0c57d134c997b6e3babca153
jin-jeffrey <49298246+jin-jeffrey@users.noreply.github.com>
2020-01-13 16:42:06 -0500

    adding starting project

---
+++ build/precache-manifest.69cb22139e910ee454b1371a4d423789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6f9e392021bb4267891d3e7b8356a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c9dbeef573817c143e9",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1a6a596bbf0c21a4efb3",
+    "url": "/static/css/main.cf959766.chunk.css"
+  },
+  {
+    "revision": "7c9dbeef573817c143e9",
+    "url": "/static/js/2.3a77dedc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3a77dedc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a6a596bbf0c21a4efb3",
+    "url": "/static/js/main.91cc6d21.chunk.js"
+  },
+  {
+    "revision": "233e64e2b074943728ef",
+    "url": "/static/js/runtime-main.cff0e806.js"
+  }
+]);
